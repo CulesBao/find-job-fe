8dc01f002128106028441e24a7820337b168a0d0
ThaiBao <baoanime2005@gmail.com>
2025-03-21 22:30:06 +0700

    feat: First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <p>hello</p>
+    </div>
+  )
+}
+
+export default App
