0bc443a08f2f04c16539d2a290efa15bffe233c8
op299 <102230373@sv1.dut.udn.vn>
2025-04-03 15:35:04 +0700

    feat: Rename

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import Register from "./pages/Register";
 import Error from "./pages/Error";
 
+import Constructions from "./pages/Constructions.jsx";
 function App() {
   return (
     <div>
       {/* <Register /> */}
-      <Error />
+      {/* <Error /> */}
+      <Constructions />;
     </div>
   );
 }
