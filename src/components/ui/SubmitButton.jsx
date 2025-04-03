06d26a2f8386da64babf1261900dd1ccea68dbfd
CulesBao <102230334@sv1.dut.udn.vn>
2025-04-03 14:54:36 +0700

    chore fix

--- src/components/ui/SubmitButton.jsx
+++ src/components/ui/SubmitButton.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from "react";
 import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
@@ -108,3 +109,34 @@ export default function SubmitButton({ Account }) {
         </>
     );
 }
+=======
+export default function SubmitButton({User}) {
+    return (
+        <button 
+            className="w-[95%] bg-[#007bff] text-white py-4 mb-[10px] border-none rounded-md cursor-pointer text-lg font-sans"
+            type="button"
+            onClick={async () => {
+                const dataUser = {
+                email: User.email,
+                password: User.password,
+                role: User.role,
+                provider: "LOCAL"
+                };
+                console.log("User data:", User);
+                try {
+                const res = await fetch("http://localhost:8080/api/auth/register", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(dataUser)
+                });
+                console.log(await res.json());
+                } catch (err) {
+                console.error(err);
+                }
+            }}
+            >
+            Create Account →
+        </button>
+    )
+}
+>>>>>>> e39897f (chore fix)
