7e266fa845846df98e2fae904cc869f5661f919b
ThaiBao <baoanime2005@gmail.com>
2025-04-03 08:04:08 +0700

    feat: add API into Register.jsx, add muiIconMaterials

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { Camera } from "lucide-react";
 import "./App.css";
-import { ButtonBase } from "@mui/material";
+import Register from "./pages/Register";
+// import Login from "./pages/Login";
+
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello !</h1>
-      <Camera className="text-red-500" size={48} />
-      <ButtonBase />
+      <Register />
     </div>
   );
 }
