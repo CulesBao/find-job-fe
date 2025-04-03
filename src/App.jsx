8c8cf50a70dfab097c5868d88c2d8eced06d9d92
op299 <102230373@sv1.dut.udn.vn>
2025-04-03 15:19:02 +0700

    feat: construction

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { Camera } from "lucide-react";
 import "./App.css";
 import { ButtonBase } from "@mui/material";
+import Error from "./pages/Error";
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello !</h1>
-      <Camera className="text-red-500" size={48} />
-      <ButtonBase />
+      <Error />
     </div>
   );
 }
