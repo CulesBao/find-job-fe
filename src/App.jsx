b7fd1abdaceb8e9648f1b3ad210027db33b7ad69
CulesBao <102230334@sv1.dut.udn.vn>
2025-04-10 15:46:29 +0700

    fix: chore fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Constructions from "./pages/Constructions";
+import Accountverify from "./pages/AccountVerify";
+import { SnackbarProvider } from "notistack";
+import { SnackbarUtilsConfigurator } from "./utils/SnackbarUtils";
 
-import SidebarCadidate from "./components/ui/SidebarCadidate";
-function App() {
+export default function App() {
   return (
-    <SidebarCadidate />
-
+    <SnackbarProvider maxSnack={3}>
+      <SnackbarUtilsConfigurator />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/constructions" element={<Constructions />} />
+          <Route path="/verify" element={<Accountverify />} />
+        </Routes>
+      </Router>
+    </SnackbarProvider>
   );
 }
-
-export default App;
