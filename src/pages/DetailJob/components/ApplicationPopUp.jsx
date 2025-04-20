969fc9d060f91c71aabd94db95cfa2a36e121f85
CulesBao <102230334@sv1.dut.udn.vn>
2025-04-20 16:59:00 +0700

    feat: upload cv

--- src/pages/DetailJob/components/ApplicationPopUp.jsx
+++ src/pages/DetailJob/components/ApplicationPopUp.jsx
@@ -1,14 +1,43 @@
-import { Dialog, DialogContent, DialogTitle, Button, TextField, Typography, IconButton } from "@mui/material";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Button,
+  TextField,
+  Typography,
+  IconButton,
+} from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import UploadFileIcon from "@mui/icons-material/UploadFile";
-import { useState } from "react";
-import { Check } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { createApplication } from "@/services/applicationService";
 
 function ApplicationPopUp({ open, onClose, title }) {
   const [resume, setResume] = useState(null);
+  const [coverLetter, setCoverLetter] = useState("");
+  const { jobId } = useParams();
 
   const handleFileChange = (e) => {
     setResume(e.target.files[0]);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (resume) {
+        URL.revokeObjectURL(resume);
+      }
+    };
+  }, [resume]);
+
+  const handleApply = async () => {
+    try {
+      await createApplication(resume, coverLetter, jobId);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      onClose();
+    }
   };
 
   return (
@@ -22,7 +51,10 @@ function ApplicationPopUp({ open, onClose, title }) {
           <CloseIcon />
         </IconButton>
       </DialogTitle>
-      <DialogContent dividers sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+      <DialogContent
+        dividers
+        sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+      >
         <Typography variant="body2">Choose Resume</Typography>
         <Button
           variant="outlined"
@@ -45,11 +77,21 @@ function ApplicationPopUp({ open, onClose, title }) {
           placeholder="Write down your biography here. Let the employers know who you are..."
           variant="outlined"
           fullWidth
+          value={coverLetter}
+          onChange={(e) => setCoverLetter(e.target.value)}
         />
 
-        <div style={{ display: "flex", justifyContent: "space-between", marginTop: 16 }}>
-          <Button variant="outlined" onClick={onClose}>Cancel</Button>
-          <Button variant="contained" color="primary" endIcon={<Check />}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: 16,
+          }}
+        >
+          <Button variant="outlined" onClick={onClose}>
+            Cancel
+          </Button>
+          <Button variant="contained" color="primary" onClick={handleApply}>
             Apply Now
           </Button>
         </div>
